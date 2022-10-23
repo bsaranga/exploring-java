70d3c0ebec3639d10ca90a261aea62f38dbd9ed9
bsaranga <bsaranga@gmail.com>
2022-10-23 11:36:33 +0530

    new changes

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
@@ -11,5 +13,10 @@ public class App
         myClass.Interact();
 
         myClass.Interact("YO YO YO!");
+
+        List<String> items = List.of("a", "b", "c", "d");
+        items.forEach(item -> {
+            System.out.println(item);
+        });
     }
 }
