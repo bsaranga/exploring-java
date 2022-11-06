23a4f7a237116cf291f990a1e702244ece344384
bsaranga <bsaranga@gmail.com>
2022-11-06 08:58:01 +0530

    spring boot and basic maven starter app

---
+++ springboot/demo/src/test/java/io/learntree/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.learntree.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
