2ba0d52c9b33bc8a21d109862c00ece95b7d16ea
bsaranga <bsaranga@gmail.com>
2022-11-12 09:14:12 +0530

    constructor injection

--- spring/conference-app-xml/src/main/java/io/conference/service/SpeakerServiceImpl.java
+++ spring/conference-app-xml/src/main/java/io/conference/service/SpeakerServiceImpl.java
@@ -9,6 +9,15 @@ public class SpeakerServiceImpl implements ISpeakerService {
     
     private ISpeakerRepository repository;
 
+    public SpeakerServiceImpl() {
+        System.out.println("No args constructor");
+    }
+
+    public SpeakerServiceImpl(ISpeakerRepository repository) {
+        System.out.println("Injection constructor");
+        this.repository = repository;
+    }
+
     public void setSpeakerRepository(ISpeakerRepository repository) {
         this.repository = repository;
     }
