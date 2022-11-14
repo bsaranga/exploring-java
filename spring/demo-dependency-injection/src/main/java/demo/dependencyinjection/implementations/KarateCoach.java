ab187031b9569e89f8e8f15c0a74f49b3c530afd
bsaranga <bsaranga@gmail.com>
2022-11-14 13:23:00 +0530

    init and destroy methods for beans

--- spring/demo-dependency-injection/src/main/java/demo/dependencyinjection/implementations/KarateCoach.java
+++ spring/demo-dependency-injection/src/main/java/demo/dependencyinjection/implementations/KarateCoach.java
@@ -13,5 +13,14 @@ public class KarateCoach implements ICoach {
     public String getDailyFortune() {
         return "10 Mana Points";
     }
+
+    // configured in the applicationContext.xml
+    public void startupMethod() {
+        System.out.println(this.toString() + ": " + "Startup method was called ...");
+    }
     
+    // configured in the applicationContext.xml
+    public void destroyMethod() {
+        System.out.println(this.toString() + ": " + "Destroy method was called...");
+    }
 }
