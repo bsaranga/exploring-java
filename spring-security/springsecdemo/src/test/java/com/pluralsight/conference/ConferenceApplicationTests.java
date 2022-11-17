2d3ff022dff8a535c2d6a430b15211c2b55b5499
bsaranga <bsaranga@gmail.com>
2022-11-17 07:14:47 +0530

    replaced with pluralsight exercise files

--- spring-security/springsecdemo/src/test/java/com/pluralsight/conference/ConferenceApplicationTests.java
+++ spring-security/springsecdemo/src/test/java/com/pluralsight/conference/ConferenceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springsecurity.demo.demospringsecurity;
+package com.pluralsight.conference;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoSpringSecurityApplicationTests {
+class ConferenceApplicationTests {
 
 	@Test
 	void contextLoads() {
