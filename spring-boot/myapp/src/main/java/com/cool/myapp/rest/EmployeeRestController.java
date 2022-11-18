abb6d4c40c36d68a76dfb27e669d3e9fd3057bce
bsaranga <bsaranga@gmail.com>
2022-11-18 18:57:17 +0530

    get by id in the rest controller

--- spring-boot/myapp/src/main/java/com/cool/myapp/rest/EmployeeRestController.java
+++ spring-boot/myapp/src/main/java/com/cool/myapp/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cool.myapp.entities.Employee;
@@ -23,5 +24,15 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return this.employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+        var employee = this.employeeService.findById(employeeId);
+        
+        if (employee == null) 
+            throw new RuntimeException(String.format("Employee with Id = %s not found.", employeeId));
+        
+        return employee;
     }
 }
