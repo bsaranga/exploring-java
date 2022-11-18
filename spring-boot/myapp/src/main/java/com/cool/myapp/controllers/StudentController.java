79f9c89164031852498926a995d3fcf57d03329d
bsaranga <bsaranga@gmail.com>
2022-11-18 14:39:46 +0530

    removed unnecessary controller code

---
+++ spring-boot/myapp/src/main/java/com/cool/myapp/controllers/StudentController.java
@@ -0,0 +1,8 @@
+package com.cool.myapp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    
+}
--- spring-boot/myapp/src/test/java/com/cool/myapp/MyappApplicationTests.java
+++ spring-boot/myapp/src/test/java/com/cool/myapp/MyappApplicationTests.java
@@ -1,13 +1,27 @@
 package com.cool.myapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyappApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
-	void contextLoads() {
+	void verifyConnectedDatabase() throws SQLException {
+		var connectionString = dataSource.getConnection().getMetaData().getURL();
+		var expectedConnectionString = "jdbc:postgresql://localhost:5432/student";
+
+		assertEquals(expectedConnectionString, connectionString);
 	}
 
 }
