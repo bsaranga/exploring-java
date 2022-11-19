9dafb7a335017fc28439bb27a73d1f713d485294
bsaranga <bsaranga@gmail.com>
2022-11-19 16:05:56 +0530

    removed unused imports

--- spring-boot/myapp/src/main/java/com/cool/myapp/configuration/SecurityConfiguration.java
+++ spring-boot/myapp/src/main/java/com/cool/myapp/configuration/SecurityConfiguration.java
@@ -1,11 +1,5 @@
 package com.cool.myapp.configuration;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
